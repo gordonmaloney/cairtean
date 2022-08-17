1ccb9414faad10715363f3a76c98fc3ea5d36244
gordonmaloney <gordonmaloney@gmail.com>
2022-08-18 00:18:33 +0100

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/cards", require("./routes/cardRoutes"));
 
-app.get('/', (req, res) => {
-    res.send('server running')
-})
+// Serve frontend
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'frontend', 'build', 'index.html')
+      )
+    );
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'));
+  }
 
 
 app.use(errorHandler);
