59211c53a565a7bf91bd93181af00bc07a37f201
gordonmaloney <gordonmaloney@gmail.com>
2022-08-17 23:56:05 +0100

    test'

--- backend/server.js
+++ backend/server.js
@@ -16,18 +16,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/cards", require("./routes/cardRoutes"));
 
-//serve frontend
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.get('/', (req, res) => {
+    res.send('server running')
+})
 
-  app.get("*", (req, res) =>
-    res.sendFile(
-      path.resolve(__dirname, "../", "frontend", "build", "index.html")
-    )
-  );
-} else {
-  app.get("/", (req, res) => res.send('Set to "production"'));
-}
 
 app.use(errorHandler);
 
